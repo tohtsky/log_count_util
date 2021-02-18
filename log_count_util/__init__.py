43527fae444c4de8e0b4f2d6c7fdae810d66d1cc
Tomoki Ohtsuki <tomoki.ohtsuki129@gmail.com>
2021-02-18 08:40:52 +0000

    add n_before method

--- log_count_util/__init__.py
+++ log_count_util/__init__.py
@@ -1,3 +1,11 @@
-from .wrapper import find_n_records_within_interval, sum_records_within_interval
+from .wrapper import (
+    find_n_records_before,
+    find_n_records_within_interval,
+    sum_records_within_interval,
+)
 
-__all__ = ["find_n_records_within_interval", "sum_records_within_interval"]
+__all__ = [
+    "find_n_records_within_interval",
+    "sum_records_within_interval",
+    "find_n_records_before",
+]
